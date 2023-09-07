23c5a9c31603badc0df46a28e2e1f7ef8cb6e6ce
Abiel Sanchez <abiel02sz@gmail.com>
2023-09-06 19:46:33 -0500

    Laboratorio4

--- laboratorio4/ganerar_matriz.php
+++ laboratorio4/ganerar_matriz.php
@@ -1,27 +1,19 @@
 <?php
-// Obtener el valor de N del formulario
 $n = $_POST['n'];
 
-// Verificar si N es un número par
-if ($n % 2 != 0) {
-    die("N debe ser un número par.");
-}
-
-// Inicializar una matriz identidad de tamaño NxN
 $matriz = array();
 for ($i = 0; $i < $n; $i++) {
     $fila = array();
     for ($j = 0; $j < $n; $j++) {
         if ($i == $j) {
-            $fila[] = 1; // Elemento de la diagonal principal
+            $fila[] = 1;
         } else {
-            $fila[] = 0; // Otros elementos
+            $fila[] = 0; 
         }
     }
     $matriz[] = $fila;
 }
 
-// Mostrar la matriz identidad en formato HTML
 echo "<h2>Matriz Identidad de Orden $n:</h2>";
 echo "<table border='1'>";
 foreach ($matriz as $fila) {
--- laboratorio4/lab4.4ds7.php
+++ laboratorio4/lab4.4ds7.php
@@ -7,29 +7,22 @@
 </head>
 <body>
 <?php
-// Crear un arreglo para almacenar números pares
+
 $arregloPares = array();
 
-// Contador para llevar el control de la cantidad de números pares ingresados
 $cantidadPares = 0;
 
-// Utilizar un bucle para llenar el arreglo con números pares
 while ($cantidadPares < 20) {
-    // Solicitar al usuario ingresar un número
     $numero = readline("Ingrese un número par: ");
 
-    // Validar si el número es par
     if ($numero % 2 == 0) {
-        // Agregar el número par al arreglo
         $arregloPares[] = $numero;
         $cantidadPares++;
     } else {
-        // Si el número es impar, solicitar al usuario ingresar un valor correcto
         echo "El número ingresado es impar. Por favor, ingrese un número par.\n";
     }
 }
 
-// Mostrar el arreglo de números pares
 echo "Arreglo de números pares:\n";
 print_r($arregloPares);
 ?>
